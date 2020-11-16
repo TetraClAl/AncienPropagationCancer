76aede9919f0917eabb7b47058f03ec162b3faf9
Romain Ballu <romain.ballu@student-cs.fr>
2020-11-16 17:03:19 +0100

    1.1

---
+++ Propagation_Cancer/test_initialisation.py
@@ -0,0 +1,16 @@
+from controleur_initialisation import *
+
+def test_ajout_astrocytes():
+    univers = create_univers(5,5)
+    res = 0
+    univers = ajout_astrocytes(univers, 0.2)
+    print(univers)
+    for i in range(5):
+        for j in range(5):
+            if univers[i,j] == 2:
+                res+=1
+    assert res == 5
+
+def test_init_univers():
+    univers = init_univers(1,1)
+    assert univers == np.array([[1]])
