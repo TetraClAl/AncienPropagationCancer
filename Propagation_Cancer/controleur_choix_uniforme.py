9ea96e40390b55cf1f7344a8a622a1492217b58f
Hamich Ibtissam <ibtissam.hamich@student-cs.fr>
2020-11-17 11:41:34 +0100

    début test choix uniforme

--- Propagation_Cancer/controleur_choix_uniforme.py
+++ Propagation_Cancer/controleur_choix_uniforme.py
@@ -2,20 +2,21 @@ import random as rd
 
 from data_main import * 
 
-def choix_uniforme_site (univers, site) :  
+def choix_uniforme_site (univers, x, y) :  
     """ choix uniforme site voisin au site en paramètre qui va devenir tumorale si il est libre
-    site: site avec une cellule tumorale """
+    x,y : coordonnée du site avec une cellule tumorale """
+    voisins = get_adj(x, y ,univers)
+    indice = rd.randint (0, len(voisins) - 1)  # on choisit un voisin de manière aléatoire 
+    a,b  = voisins[indice] 
 
-    indice = rd.randint (0, 5)  # on choisit un voisin de manière aléatoire 
-    site_tumorale = voisins(site)[indice] # seulement si la fonction disponible renvoie bien une liste
-
-    if get_cellC(site_tumorale , univers ) == 0 : 
-        set_cellC(site_tumorale, 1, univers)
-        set_cellC(site, 0, univers)
+    if get_cell(a, b , univers ) == 0 : 
+        set_cell(a, b , 1, univers)
+        set_cell(x ,y, 0, univers)
 
 def choix_uniforme (univers) : 
     """ applique la fonction choix_uniforme_site a tous les sites avec une cellule tumorale """
     for i in range (len(univers)) : 
         for j in range (len(univers[0])) : #on parcourt tout l'univers
-            if get_cellC ((i,j), univers) == 1 : #si site est occupé par cellule tumorale elle peut migrer
-                choix_uniforme_site (univers, site)
+            if get_cell (i,j, univers) == 1 : #si site est occupé par cellule tumorale elle peut migrer
+                choix_uniforme_site (univers, i ,j)
+
