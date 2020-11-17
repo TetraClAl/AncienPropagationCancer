c33568859fcddaa852bcef41d41661fdc5c85a74
Le Brun Victor <victor.lebrun@student-cs.fr>
2020-11-17 01:39:34 +0100

    ajout commentaires, réorganisation fichiers

--- Propagation_Cancer/data_main.py
+++ Propagation_Cancer/data_main.py
@@ -1,9 +1,27 @@
 from data_base import *
 from data_advanced import *
 
-# 0 = Vide
-# 1 = Tumeur
-# 2 = Astrocyte
+# -------
+#
+# Présentation des conventions pour les variables TODO : faire un .md ou wiki (.md préférable vu le temps)
+#
+# Type env :
+# [0] => univers (np.array(x, y))
+#           -> 0 = vide
+#           -> 1 = Tumeur
+#           -> 2 = Astrocyte
+# [1] => folder ([index] => groupe)
+#
+# Type groupe :
+# [0] => composants ([(x, y)] => coordonnées cellules composants le groupe)
+# [1] => connexions ([(x, y)] => coordonnées cellules adjacentes au groupe)
+#
+# (NB : Les listes de cellules seront sûrement classées à un moment pour accélérer les fusions sans doublons)
+# (idée : Algo de tri fusion sélectionné, car il se fera naturellement au fûr et à mesure de la fusion des listes)
+# TODO : Se coucher avant 2h du mat
+# TODO : Mettre à jour le marquage des fonctionnalités en fonction de
+#
+# -------
 
 
 # Fonctionnalité 0.1
@@ -30,13 +48,18 @@ def set_cell(x, y, value, univers):
 
 
 def check_list(l1, univers):
+    """ Elimine les éléments non valides de la liste (hors de l'univers) mais pas les doublons. """
     return s_check_list(l1, univers)
 
 
 # Fonctionnalité 2
 
 
-def get_groupe(coord, univers):
+def create_env(x, y):
+    """ Crée un environnement de taille (x, y). """
+
+
+def get_groupe(coord, env):
     """ Retourne l'index du groupe de la cellule (x, y). """
 
 
@@ -45,11 +68,11 @@ def get_adj(x, y, univers):
     return s_get_adj(x, y, univers)
 
 
-def get_adj_groupe(index, univers):
+def get_adj_groupe(index, env):
     """ Retourne la liste des coordonnées des cellules adjacentes au groupe index. """
 
 
-def get_membre_groupe(index, univers):
+def get_membre_groupe(index, env):
     """ Retourne la liste des coordonnées des cellules du groupe index. """
 
 
