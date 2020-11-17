c7cffa9076bccb8a1607b93ed088be9e56242f90
Hamich Ibtissam <ibtissam.hamich@student-cs.fr>
2020-11-17 10:22:38 +0100

    modification choix uniforme

--- Propagation_Cancer/controleur_choix_uniforme.py
+++ Propagation_Cancer/controleur_choix_uniforme.py
@@ -9,13 +9,13 @@ def choix_uniforme_site (univers, site) :
     indice = rd.randint (0, 5)  # on choisit un voisin de manière aléatoire 
     site_tumorale = voisins(site)[indice] # seulement si la fonction disponible renvoie bien une liste
 
-        if get_cellC(site_tumorale , univers ) == 0 : 
-            set_cellC(site_tumorale, 1, univers)
-            set_cellC(site, 0, univers)
+    if get_cellC(site_tumorale , univers ) == 0 : 
+        set_cellC(site_tumorale, 1, univers)
+        set_cellC(site, 0, univers)
 
 def choix_uniforme (univers) : 
-""" applique la fonction choix_uniforme_site a tous les sites avec une cellule tumorale """
+    """ applique la fonction choix_uniforme_site a tous les sites avec une cellule tumorale """
     for i in range (len(univers)) : 
-            for j in range (len(univers[0])) : #on parcourt tout l'univers
-                if get_cellC ((i,j), univers) == 1 : #si site est occupé par cellule tumorale elle peut migrer
-                    choix_uniforme_site (univers, site)
+        for j in range (len(univers[0])) : #on parcourt tout l'univers
+            if get_cellC ((i,j), univers) == 1 : #si site est occupé par cellule tumorale elle peut migrer
+                choix_uniforme_site (univers, site)
---
+++ Propagation_Cancer/test_controleur_choix_uniforme.py
@@ -0,0 +1,5 @@
+from controleur_choix_uniforme import *
+from pytest import *
+
+assert True 
+print ('le premier test est un succès')
