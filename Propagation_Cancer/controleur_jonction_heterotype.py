93faa4644e685d6af639536a662e3accdf0cf11f
Hamich Ibtissam <ibtissam.hamich@student-cs.fr>
2020-11-18 11:48:03 +0100

    fonctionnalité 2.2 non testé

--- Propagation_Cancer/controleur_jonction_heterotype.py
+++ Propagation_Cancer/controleur_jonction_heterotype.py
@@ -4,12 +4,26 @@ from data_main import *
 
 def get_adj_tri(x, y, env) : 
     """ renvoie la liste des sites adjacents occupés par un astrocyte"""
+    astro = []
+    univers = env[0]
+    voisins = get_adj(x, y, univers)
+    for site in voisins : # on parcourt tous les voisins
+        x,y = site 
+        if get_cell(x, y, univers) == 2 : #si ce sont des astrocytes, on les rajoute à la liste 
+            astro.append (site)
     return asto
 
 
 
 def get_adj_vide (x, y, env) : 
     """ renvoie la liste des sites adjacents vides"""
+    vide = []
+    univers = env[0]
+    voisins = get_adj(x, y, univers)
+    for site in voisins : # on parcout tous les voisins
+        x,y = site 
+        if get_cell(x, y, univers) == 0 : #si ils sont vides on les rajoute à la liste
+            vide.append (site)
     return vide 
 
 
@@ -38,6 +52,6 @@ def jonction_heterotype (env) :
     """ applique la fonction jonciton_heterotype_site a tous les sites avec une cellule tumorale """
     for i in range (len(env[0])) : 
         for j in range (len(env[0][0])) : #on parcourt tout l'univers
-            
+
             if get_cell (i,j, env[0]) == 1 : #si site est occupé par cellule tumorale elle peut migrer
                 jonction_heterotype_site (env, i ,j)
